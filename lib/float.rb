7462a4c703d6642d99acc9633d45726cd75ecf65
Krystan Menne and Lauryn Davis <lryndavis@gmail.com>
2016-02-03 16:04:14 -0800

    add method to convert floats into mixed fraction strings

--- lib/float.rb
+++ lib/float.rb
@@ -1,9 +1,16 @@
 class Float
   def to_rational_string
     integer_component = to_i
-    if (integer_component != self) && (integer_component > 0)
-      fraction = (self - integer_component).to_r
-      "#{integer_component} #{fraction}"
+
+    if integer_component != self
+      fraction = (self - integer_component).to_r.to_s
+
+      if integer_component > 0
+        "#{integer_component} #{fraction}"
+      elsif integer_component == 0
+        "#{fraction}"
+      end
+
     else
       integer_component.to_s
     end
